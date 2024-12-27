5861b1e677f61ef62f73df5e4bd6a73227718083
Ahmed Samir <asamir2211997@gmail.com>
2024-12-27 16:44:55 +0200

    add error message

--- src/android/FFMpeg.java
+++ src/android/FFMpeg.java
@@ -25,7 +25,7 @@ public class FFMpeg extends CordovaPlugin {
                     if (returnCode == RETURN_CODE_SUCCESS)
                         callbackContext.success(result);
                     else
-                        callbackContext.error("Error Code: " + returnCode);
+                        callbackContext.error("Error Code: " + returnCode + "," + Config.getLastCommandOutput());
                 }
             });
             return true;
